e9a04d48930b8417ee006a2272a0111b9f40057f
Marc <marc.ziegler@smaint.de>
2022-05-19 19:46:38 +0200

    Temp Worksave

--- MultidimentionalIntegrationError.py
+++ MultidimentionalIntegrationError.py
@@ -9,20 +9,20 @@ import sobol as sob
 
 #Configuration Variables
 SEED = 1337
-FUNC = lambda input: [math.sin(x) for x in input]
+FUNC = lambda input: 1 if math.fsum([math.pow(x, 2) for x in input]) <= 1 else 0
 BASE = 2 #Halton sequence base
-DIM = 5
+DIM = 6
 SKIP = 0
-REALAREA = 2
-DOMAIN = [(0, math.pi) for _ in DIM]
-N = [i*100 for i in range(1, 50)]
+REALAREA = math.pow(math.pi, 3)/6 #for hypersphere in 6 DIM
+DOMAIN = (-1, 1) #Cube domain
+N = [i*100 for i in range(1, 10)] #Number of samples
 TRIALS = 10
 
 def GeneratePrimes(n):
-    N = n * int(math.log(n) + math.log(math.log(n)))
-    a = range(2, N)
+    no = n * int(math.log(n) + math.log(math.log(n)))
+    a = range(2, no)
     for i in range(int(n ** .5) + 1):
-        a = list(filter(lambda x: x % a[i] or x == a[i], a))
+        a = list(filter(lambda o: o % a[i] or o == a[i], a))
     return a
 
 def GenerateHaltonGenerator(b):
@@ -39,22 +39,22 @@ def GenerateHaltonGenerator(b):
             n = (b + 1) * y - x
         yield n / d
 
-def GenerateHaltonSequence(N, k):
-    primes = GeneratePrimes(k + 1)
+def GenerateHaltonSequences(n, k):
+    primes = GeneratePrimes(n + 20) #TODO this behaves weird
     seq = []
-    for i in range(k):
+    for i in range(0, n):
         gen = GenerateHaltonGenerator(primes[i])
-        seq += [[next(gen) for _ in range(N)]]
+        seq += [[next(gen) for _ in range(k)]]
     return seq
 
-def GenerateSobolSequences(N ,k):
-    return sob.sample(k, N, SKIP)
+def GenerateSobolSequences(n, k):
+    return sob.sample(k, n, SKIP)
 
-def GenerateRandomSequence(N ,k):
-    return [[rdm.random() for _ in N] for _ in k]
+def GenerateRandomSequences(n, k):
+    return [[rdm.random() for _ in range(k)] for _ in range(n)]
 
-def CalculateDomainVolume(dom):
-    return math.prod([x[1] - x[0] for x in dom])
+def CalculateDomainVolume():
+    return math.pow(DOMAIN[1] - DOMAIN[0], DIM)
 
 def MDMCIntegralError(generator):
     averages, lbounds, ubounds = [], [], []
@@ -62,8 +62,8 @@ def MDMCIntegralError(generator):
         avgSum, maxE, minE = 0.0, 0.0, sys.float_info.max
         for _ in range(TRIALS):
             samples = generator(n, DIM)
-            samples = [[FUNC(math.dist(DOMAIN[dindex][sindex])*samples[dindex][sindex]) for sindex in range(n)] for dindex in range(DIM)]
-            error = math.fabs(REALAREA - CalculateDomainVolume(DOMAIN) * math.fsum(samples) / n)
+            samples = [FUNC([(DOMAIN[1]-DOMAIN[0])*s for s in nn]) for nn in samples]
+            error = math.fabs(REALAREA - CalculateDomainVolume() * math.fsum(samples) / n)
             avgSum += error
             maxE = max(error, maxE)
             minE = min(error, minE)
@@ -77,9 +77,48 @@ def MDRiemannIntegralError():
     return []
 
 if __name__ == '__main__':
+    rdm.seed(SEED)
+
     print(GeneratePrimes(5))
-    print(GenerateHaltonSequence(5, 5))
-    print(GenerateSobolSequences(5, 5))
-    print(np.shape(GenerateHaltonSequence(5, 5)))
-    print(np.shape(GenerateSobolSequences(5, 5)))
-    print(MDMCIntegralError(GenerateSobolSequences))
+    print(GenerateHaltonSequences(6, 5))
+    print(GenerateSobolSequences(6, 5))
+    print(np.shape(GenerateHaltonSequences(6, 5)))
+    print(np.shape(GenerateSobolSequences(6, 5)))
+    #print(MDMCIntegralError(GenerateSobolSequences))
+
+    randomRes = MDMCIntegralError(GenerateRandomSequences)
+    sobolRes = MDMCIntegralError(GenerateSobolSequences)
+    haltonRes = MDMCIntegralError(GenerateHaltonSequences)
+
+    # x axis values
+    x = N
+    # corresponding y axis values
+    yrandom = randomRes[0]
+    yrandome = randomRes[1]
+    ysobol = sobolRes[0]
+    ysobole = sobolRes[1]
+    yhalton = haltonRes[0]
+    yhaltone = haltonRes[1]
+
+    # plotting the points
+    graph = plt.figure()
+    randomPlot = graph.add_subplot(1, 1, 1)
+    randomPlot.errorbar(x, yrandom, yerr=yrandome, ecolor='yellow', capsize=4.5, elinewidth=3.0, color='tab:green',
+                    label='Random Sampling')
+    sobolPlot = graph.add_subplot(1, 1, 1)
+    sobolPlot.errorbar(x, ysobol, yerr=ysobole, ecolor='red', capsize=4.5, elinewidth=3.0, color='tab:red',
+                    label='Sobol Sampling')
+    haltonPlot = graph.add_subplot(1, 1, 1)
+    haltonPlot.errorbar(x, yhalton, yerr=yhaltone, ecolor='blue', capsize=4.5, elinewidth=3.0, color='tab:blue',
+                    label='Halton Sampling')
+    plt.legend(loc='upper right')
+    # naming the x axis
+    plt.xlabel('Number of subintervals / samples N')
+    # naming the y axis
+    plt.ylabel('Absolute error')
+
+    # giving a title to my graph
+    plt.title('Absolute integration error of a 6 Dimensional Hypersphere')
+
+    # function to show the plot
+    plt.show()
